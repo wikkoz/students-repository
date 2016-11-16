454cebcbe7467a19b913881fa6002f130bb10e55
wikkoz <wiktorv94@o2.pl>
2016-11-16 19:42:55 +0100

    - fix running spring boot on tomcat

--- repository-web/src/main/java/com/web/Application.java
+++ repository-web/src/main/java/com/web/Application.java
@@ -5,13 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude={LiquibaseAutoConfiguration.class})
 @ComponentScan("com")
 @PropertySource("classpath:/repository.properties")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
