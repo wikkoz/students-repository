27c4189e4226e9487d8ae05487a276553b3135a3
wikkoz <wiktorv94@o2.pl>
2016-12-17 13:37:16 +0100

    cas

--- security/src/main/java/com/secuirty/config/WebSecurityConfig.java
+++ security/src/main/java/com/secuirty/config/WebSecurityConfig.java
@@ -1,23 +1,27 @@
 package com.secuirty.config;
 
 import com.database.repository.UserRepository;
-import com.google.common.collect.Lists;
+import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.cas.ServiceProperties;
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -32,35 +36,73 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserRepository userRepository;
 
+    @Bean
+    public ServiceProperties serviceProperties() {
+        ServiceProperties properties = new ServiceProperties();
+        properties.setSendRenew(false);
+        properties.setService("https://localhost:8080/login/cas");
+        return properties;
+    }
 
     @Bean
     public AuthenticationUserDetailsService<CasAssertionAuthenticationToken> customUserDetailsService() {
         return new CasUserDetailsService();
     }
 
-//    @Bean
-//    public CasAuthenticationProvider casAuthenticationProvider() {
-//        CasAuthenticationProvider casAuthenticationProvider = new CasAuthenticationProvider();
-//        casAuthenticationProvider.setAuthenticationUserDetailsService(customUserDetailsService());
-////TODO cas
-//        return casAuthenticationProvider;
-//    }
+    @Bean
+    public CasAuthenticationProvider casAuthenticationProvider() {
+        CasAuthenticationProvider casAuthenticationProvider = new CasAuthenticationProvider();
+        casAuthenticationProvider.setAuthenticationUserDetailsService(customUserDetailsService());
+        casAuthenticationProvider.setServiceProperties(serviceProperties());
+        casAuthenticationProvider.setTicketValidator(new Cas20ServiceTicketValidator("https://merkury.elka.pw.edu.pl/cas"));
+        casAuthenticationProvider.setKey("test");
+        return casAuthenticationProvider;
+    }
 
+    @Bean
+    public AuthenticationEntryPoint casAuthenticationEntryPoint() {
+        CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
+        entryPoint.setLoginUrl("https://merkury.elka.pw.edu.pl/cas/login");
+        entryPoint.setServiceProperties(serviceProperties());
+        return entryPoint;
+    }
+
+    @Bean
+    public SessionAuthenticationStrategy sessionStrategy() {
+        SessionAuthenticationStrategy sessionStrategy = new SessionFixationProtectionStrategy();
+        return sessionStrategy;
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        AuthenticationEntryPoint casAEP = casAuthenticationEntryPoint();
+        return casAEP;
+    }
     @Override
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(new AuthenticationProvider() {
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                return new UsernamePasswordAuthenticationToken("ADMIN", "ADMIN", Lists.newArrayList(()->"ADMIN"));
-            }
+//        auth.authenticationProvider(new AuthenticationProvider() {
+//            @Override
+//            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//                return new UsernamePasswordAuthenticationToken("ADMIN", "ADMIN", Lists.newArrayList(()->"ADMIN"));
+//            }
+//
+//            @Override
+//            public boolean supports(Class<?> authentication) {
+//                return true;
+//            }
+//        });
+        auth.authenticationProvider(casAuthenticationProvider());
+    }
 
-            @Override
-            public boolean supports(Class<?> authentication) {
-                return true;
-            }
-        });
-//        auth.authenticationProvider(casAuthenticationProvider());
+    @Bean
+    public CasAuthenticationFilter authenticationFilter() throws Exception {
+        CasAuthenticationFilter filter = new CasAuthenticationFilter();
+        AuthenticationManager authenticationManager = authenticationManagerBean();
+        filter.setAuthenticationManager(authenticationManager);
+        filter.setSessionAuthenticationStrategy(sessionStrategy());
+
+        return filter;
     }
 
     @Override
@@ -68,7 +110,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
-                .httpBasic()
+                .addFilter(authenticationFilter()).exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint())
                 .and()
                 .csrf().disable();
     }
